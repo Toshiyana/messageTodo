bd156867f3d6da8ecb3ba037f0588195169123a3
Toshiki Yanagimoto <toshiyana0306@gmail.com>
2022-06-02 00:10:27 +0900

    refactor: add Task & MessageManager

---
+++ messageTodo/Model/MessageManager.swift
@@ -0,0 +1,12 @@
+//
+//  MessageManager.swift
+//  messageTodo
+//
+//  Created by Toshiyana on 2022/06/02.
+//
+
+import Foundation
+
+struct MessageManager {
+    
+}
---
+++ messageTodo/Model/TaskManager.swift
@@ -0,0 +1,12 @@
+//
+//  TaskManager.swift
+//  messageTodo
+//
+//  Created by Toshiyana on 2022/06/02.
+//
+
+import Foundation
+
+struct TaskManager {
+    
+}
